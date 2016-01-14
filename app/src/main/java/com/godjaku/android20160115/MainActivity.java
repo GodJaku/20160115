82fd264ea8d870df8246ed3b3c84b1cde15f7dbe
GodJaku <0101rgb.test@gmail.com>
2016-01-15 01:33:56 +0900

    add fragment

--- app/src/main/java/com/godjaku/android20160115/MainActivity.java
+++ app/src/main/java/com/godjaku/android20160115/MainActivity.java
@@ -1,13 +1,40 @@
 package com.godjaku.android20160115;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import com.godjaku.android20160115.fragment.MainFragment;
+
+public class MainActivity extends FragmentActivity {
+    private FragmentTransaction mFrgTransaction= null;
+
+    private MainFragment mMain= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        makeView();
+        moveFragment();
+    }
+    @Override
+    public void onPause(){ super.onPause(); }
+
+    private void makeView(){
+        mMain= new MainFragment();
+    }
+
+    public void moveFragment(){
+        mFrgTransaction= getSupportFragmentManager().beginTransaction();
+        mFrgTransaction.add(R.id.container, mMain);
+        mFrgTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/godjaku/android20160115/fragment/MainFragment.java
@@ -0,0 +1,35 @@
+package com.godjaku.android20160115.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.godjaku.android20160115.R;
+
+public class MainFragment extends Fragment {
+    private View wholeView= null;
+
+    public MainFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle data){
+        wholeView= inflater.inflate(R.layout.fragment_main, null);
+        return wholeView;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        makeView();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    private void makeView(){
+
+    }
+}
